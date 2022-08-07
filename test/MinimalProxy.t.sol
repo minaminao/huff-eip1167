5edc757854478ca572bab9ede62a76a9b665de6c
minami <minaminaoy@gmail.com>
2022-08-07 20:10:30 +0900

    Add MinimalProxy with vanity address

--- test/MinimalProxy.t.sol
+++ test/MinimalProxy.t.sol
@@ -6,9 +6,12 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 contract SimpleStoreTest is Test {
+    SimpleStore public simpleStore;
     SimpleStore public minimalProxy;
     SimpleStore public minimalProxyUsingLabel;
-    SimpleStore public simpleStore;
+    SimpleStore public minimalProxyWithVanityAddress;
+    address public vanityAddress = 0x0000000000060708091011121314151617181920;
+    string public vanityAddressString = "0x060708091011121314151617181920";
 
     function setUp() public {
         simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
@@ -21,6 +24,12 @@ contract SimpleStoreTest is Test {
         // 46 bytes
         minimalProxyUsingLabel = SimpleStore(
             new HuffConfig().with_addr_constant("ADDRESS", address(simpleStore)).deploy("MinimalProxyUsingLabel")
+        );
+
+        // 40 bytes (vanity address: 0x060708091011121314151617181920)
+        vm.etch(vanityAddress, address(simpleStore).code);
+        minimalProxyWithVanityAddress = SimpleStore(
+            new HuffConfig().with_constant("ADDRESS", vanityAddressString).with_constant("JUMPI_DST", "0x26").deploy("MinimalProxy")
         );
     }
 
@@ -36,6 +45,13 @@ contract SimpleStoreTest is Test {
         console.log(value);
         console.log(minimalProxyUsingLabel.getValue());
         assertEq(value, minimalProxyUsingLabel.getValue());
+    }
+
+    function testMinimalProxyWithVanityAddress(uint256 value) public {
+        minimalProxyWithVanityAddress.setValue(value);
+        console.log(value);
+        console.log(minimalProxyWithVanityAddress.getValue());
+        assertEq(value, minimalProxyWithVanityAddress.getValue());
     }
 }
 
